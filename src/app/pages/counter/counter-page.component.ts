c5753e546b124421cbeb09e6f21fbe6ca1bd7952
Manuel Llorens <pixelartrecompilation@gmail.com>
2025-05-10 12:31:47 +0200

    src/app/pages/counter/counter-page.component.ts: signal Imported, unique typo

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,17 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 
 @Component({
-    templateUrl: './counter-page.component.html'
+    templateUrl: './counter-page.component.html',
+    styles: `
+        button{
+            padding: 5px;
+            margin: 5px 10px;
+            width: 75px;
+        }
+    `,
 })
 export class CounterPageComponent{
     counter = 0;
+    counterSignal = signal(0);
 
     increaseBy(value: number){
         this.counter += value;
     }
-
-
     resetCounter(){
-this.counter = 0;
+        this.counter = 0;
+        this.counterSignal.set(0);
     }
 }
